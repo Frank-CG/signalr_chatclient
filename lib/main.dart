bf530b6087e65110afbefa2fc6990bff650e8e1a
frankg <frankg@sliq.com>
2019-07-29 12:03:57 -0400

    signalr demo client.

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:chatclient/app.dart';
+import 'package:flutter/material.dart';
+
+
+const kChatServerUrl = "http://yongharmonyvm.canadaeast.cloudapp.azure.com/SignalRChatTest";
+
+void main() {
+  runApp(App());
+}
